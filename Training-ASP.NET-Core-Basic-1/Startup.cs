80374f37b99d8515f67754f2b020cdf8b0341dc1
Dung Ho <mclanren156@gmail.com>
2020-12-07 00:16:05 +0700

    PostTodoItem

--- Training-ASP.NET-Core-Basic-1/Startup.cs
+++ Training-ASP.NET-Core-Basic-1/Startup.cs
@@ -1,16 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Training_ASP.NET_Core_Basic_1.Models;
 
 namespace Training_ASP.NET_Core_Basic_1
 {
@@ -26,6 +21,7 @@ namespace Training_ASP.NET_Core_Basic_1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
